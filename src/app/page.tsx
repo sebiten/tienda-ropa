4e54af8c7066a32f29392d31ef7104eba504b271
Sebiten <sebdevspace@gmail.com>
2024-02-06 11:53:53 -0300

    starting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@/components/ui/button"
-
+import NavBar from "@/components/ui/NavBar";
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@radix-ui/react-navigation-menu";
 
 export default function Home() {
   return (
     <main>
+      <NavBar />
       <Button>Hola</Button>
     </main>
   );
